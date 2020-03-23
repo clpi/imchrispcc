17f814842906f5ca8a9b7222b09c6e09a69faf6e
Chris P <chrisp1877@gmail.com>
2020-03-23 03:44:44 -0700

    updating about and contact

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const About = () => (
       <h1 className="about">about me</h1>
       <p className="description">What about me?</p>
     <p>
-        I am a recent graduate at the University of Washington Seattle's Materials Scence & Engineering program. I'm a physical engineer by training strongly leaning into (I wouldn't quite call it pivoting!) towards software -- specifically, full stack web development and data science. I'll update each of the relevant sections below as I continue to work on the site!</p>
+        I am a recent graduate at the University of Washington Seattle's Materials Scence & Engineering program. I'm a materials engineer by training with a strong proclivity and supplemental skillset in the realms of data science, full stack web development, and systems engineering. I'll update each of the relevant sections below as I continue to work on the site!</p>
       <div className="card" id="aboutCard">
         <h2>Materials</h2>
       </div>
@@ -17,7 +17,7 @@ const About = () => (
         <h2>Data</h2>
       </div>
       <div className="card" id="aboutCard">
-        <h2>and bears, oh my</h2>
+        <h2>Nanotech</h2>
       </div>
 
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,18 +8,18 @@ const Contact = () => (
     <SEO title="contact" keywords={[`gatsby`, `application`, `react`]} />
         <h1 className="contact">contact</h1>
 
-      <p>If you need to contact me, please reach out to me at any of my following social media accounts!</p>
+    <p>If you need to contact me, please reach out to me at any of my following social media accounts! Alternatively, email me at <a href="mailto:chris@cusys.io" class="contact">chris@cusys.io</a>.</p>
 
       <div className="card" id="contactCard">
-            <h3>Github</h3>
+        <a href="http://github.com/pecusys" class="contact"><h3>Github</h3></a>
       </div>
 
     <div className="card" id="contactCard">
-            <h3>LinkedIn</h3>
+      <a href="https://www.linkedin.com/in/chris-pecunies-641b8a101/" class="contact"><h3>LinkedIn</h3></a>
       </div>
 
         <div className="card" id="contactCard">
-        <h3>Twitter</h3>
+          <h3>Twitter</h3>
       </div>
 
       </Layout>
